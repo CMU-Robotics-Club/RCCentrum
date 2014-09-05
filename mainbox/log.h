a6384f76d652681cfc2f23acc7eddfb33946b567
Brent Strysko <bstrysko@BSTRYSKO-LAPTOP.WV.CC.CMU.EDU>
2014-09-05 14:23:22 -0400

    Removed tooltron code and extracted existing core CRM code from Tooltron.

--- mainbox/log.h
+++
@@ -1,7 +0,0 @@
-#ifndef LOG_H
-#define LOG_H
-
-void log_print(const char *fmt, ...);
-void log_perror(const char *s);
-
-#endif
--- toolbox/current.h
+++
@@ -1,7 +0,0 @@
-#ifndef CURRENT_H
-#define CURRENT_H
-
-void current_init();
-unsigned int current_read();
-
-#endif
