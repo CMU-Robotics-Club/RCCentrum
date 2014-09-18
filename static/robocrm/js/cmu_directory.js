151f8f8278498927703c632466256ae87e3e058d
Brent Strysko <brentstrysko@brentstrysko.com>
2014-09-18 18:18:01 -0400

    Fixed Faculty bug in cmu_directory.js

--- static/robocrm/js/cmu_directory.js
+++ static/robocrm/js/cmu_directory.js
@@ -9,57 +9,76 @@ $(document).ready(function(){
 
     var info = cmu.directory.findAndrewId(andrewID);
 
+    var first_name = "";
+    var last_name = "";
+    var email = "";
+    var major = "";
+    var grad_year = "";
+    var class_level = null;
+
     if(info) {
-      console.log(info);
+      first_name = info["first_name"];
+      last_name = info["last_name"];
+      email = info["email"];
+      major = info["department"];
 
-      $("#id_first_name").val(info["first_name"]);
-      $("#id_last_name").val(info["last_name"]);
-      $("#id_email").val(info["email"]);
-      $("#id_robouser-0-major").val(info["department"]);
+      var role = info["role"];
+      var student_class = info["student_class"];
+      var student_level = info["student_level"];
 
-      var year_offset = 0;
+      if(role == "Student") {
+        var year_offset = null;
 
-      switch(info["student_class"]) {
-        case "Senior": {
-          year_offset = 0;
-          break;
+        switch(info["student_class"]) {
+          case "Senior": {
+            year_offset = 0;
+            break;
+          }
+          case "Junior": {
+            year_offset = 1;
+            break;
+          }
+          case "Sophmore": {
+            year_offset = 2;
+            break;
+          }
+          case "Freshman": {
+            year_offset = 3;
+            break;
+          }
         }
-        case "Junior": {
-          year_offset = 1;
-          break;
+
+        var date = new Date();
+        var current_year = date.getFullYear();
+        // Spring if current month is less than June else new year
+        var is_spring = (date.getMonth() < 5);
+
+        if(!is_spring) {
+          //If in the Fall graduation is following year
+          year_offset += 1;
         }
-        case "Sophmore": {
-          year_offset = 2;
-          break;
-        }
-        case "Freshman": {
-          year_offset = 3;
-          break;
-        }
+
+        grad_year = current_year + year_offset;
       }
 
-      var date = new Date();
-      var current_year = date.getFullYear();
-      // Spring if current month is less than June else new year
-      var is_spring = (date.getMonth() < 5);
+      var class_level = null;
 
-      if(!is_spring){
-        //If in the Fall graduation is following year
-        year_offset += 1;
-      }
-
-      var grad_year = current_year + year_offset;
-
-      $("#id_robouser-0-grad_year").val(grad_year);
-
-
-      var class_level = 1;
-
-      if(info["student_level"] == "Undergrad") {
+      if(student_level == "Undergrad") {
         class_level = 0;
       }
-
-      $("#id_robouser-0-class_level>option:eq(" + class_level + ")").prop('selected', true);
+      else if(student_level == "Graduate") {
+        class_level = 1;
+      }
+      else if(role == "Faculty") {
+        class_level = 2;
+      }
     }
+    
+    $("#id_first_name").val(first_name);
+    $("#id_last_name").val(last_name);
+    $("#id_email").val(email);
+    $("#id_robouser-0-major").val(major);
+    $("#id_robouser-0-grad_year").val(grad_year);
+    $("#id_robouser-0-class_level>option:eq(" + class_level + ")").prop('selected', true);
   });
 });
