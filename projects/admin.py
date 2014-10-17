8330cefb358d9d81287559c04ee8c1e282efc146
Brent Strysko <brentstrysko@brentstrysko.com>
2014-10-16 20:07:00 -0400

    Only superusers and officers can change project display.

--- projects/admin.py
+++ projects/admin.py
@@ -2,9 +2,23 @@ from django.contrib import admin
 from projects.models import Project
 
 class ProjectAdmin(admin.ModelAdmin):
+
   fields = ('name', 'current_image', 'image', 'blurb', 'description', 'website', 'leaders', 'display', 'private_key', 'last_api_activity')
   filter_horizontal = ('leaders',)
   readonly_fields = ['current_image', 'last_api_activity']
+
+  # TODO: find be a better way to do this function
+  def get_form(self, request, obj=None, **kwargs):
+    user = request.user
+
+    if not user.is_superuser and not user.groups.filter(name='officers').exists():      
+      if 'display' not in self.readonly_fields:
+        self.readonly_fields.append('display')
+    else:
+      if 'display' in self.readonly_fields:
+        self.readonly_fields.remove('display')
+
+    return super().get_form(request, obj, **kwargs)
 
   def get_queryset(self, request):
     qs = super().get_queryset(request)
