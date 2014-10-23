7fea46cd1cb14e08e85c57b23a9cbceabd11a279
Brent Strysko <brentstrysko@brentstrysko.com>
2014-10-23 09:58:56 -0400

    Webcams and SocialMedias now plural in API.

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import include, patterns, url
 router = routers.DefaultRouter()
 
 router.register(r'login', LoginViewSet, base_name="login")
-router.register(r'webcam', WebcamViewSet)
+router.register(r'webcams', WebcamViewSet)
 router.register(r'datetime', DateTimeViewSet, base_name="datetime")
 router.register(r'users', RoboUserViewSet)
 router.register(r'officers', OfficerViewSet)
@@ -13,7 +13,7 @@ router.register(r'projects', ProjectViewSet)
 router.register(r'channels', ChannelViewSet, base_name="channels")
 router.register(r'calendar', CalendarViewSet, base_name="calendar")
 router.register(r'sponsors', SponsorViewSet)
-router.register(r'socialmedia', SocialMediaViewSet)
+router.register(r'socialmedias', SocialMediaViewSet)
 
 router.register(r'magnetic', MagneticViewSet, base_name="magnetic")
 router.register(r'rfid', RFIDViewSet, base_name="rfid")
