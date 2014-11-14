a4eaad11006f3e5e8b32d8fabdf79853077f5d73
Brent Strysko <brentstrysko@brentstrysko.com>
2014-11-14 10:20:02 -0500

    Fixed bug in API Exception handler.

--- api/exception_handler.py
+++ api/exception_handler.py
@@ -3,9 +3,10 @@ from rest_framework.views import exception_handler
 def api_exception_handler(exc):
   response = exception_handler(exc)
 
-  if response is not None and hasattr(exc, 'errno'):
-    response.data['errno'] = exc.errno
-  else:
-    response.data['errno'] = -1
+  if response is not None and hasattr(response, 'data'):
+    if hasattr(exc, 'errno'):
+      response.data['errno'] = exc.errno
+    else:
+      response.data['errno'] = -1
 
   return response
