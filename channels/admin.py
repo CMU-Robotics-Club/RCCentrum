1ef3529924d0388560080ae2e69e92698a769b03
Brent Strysko <brentstrysko@brentstrysko.com>
2014-11-18 19:18:04 -0500

    Fixed add channel bug from last commit.

--- channels/admin.py
+++ channels/admin.py
@@ -3,7 +3,13 @@ from .models import Channel
 
 class ChannelAdmin(admin.ModelAdmin):
   fields = ('id', 'name', 'value', 'created', 'updated', 'active', )
-  readonly_fields = ['id', 'name', 'created', 'updated', 'active', ]
+  readonly_fields = ['id', 'created', 'updated', 'active', ]
   list_display = ('id', 'name', 'value', 'created', 'updated', 'active', )
+
+  def get_readonly_fields(self, request, obj=None):
+    if obj:
+      return super().get_readonly_fields(request, obj) + ['name']
+    else:
+      return super().get_readonly_fields(request, obj)
 
 admin.site.register(Channel, ChannelAdmin)
