16955d897302dba851e8a1b630773620cc1d91a0
Brent Strysko <brentstrysko@brentstrysko.com>
2014-12-18 13:43:51 -0500

    Created some basic tests for limited parts of API.

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,56 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from projects.models import Project
 
-# Create your tests here.
+class AuthenticationTests(APITestCase):
+
+  def test_forbidden(self):
+    response = self.client.get("/api/")
+    self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+  def test_success(self):
+    project = Project(name="TestProject", private_key="secret")
+    project.save()
+
+    headers = {
+      'HTTP_PUBLIC_KEY': project.id,
+      'HTTP_PRIVATE_KEY': project.private_key
+    }
+
+    response = self.client.get("/api/", {}, **headers)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class AuthenticatedAPITestCase(APITestCase):
+
+  def setUp(self):
+    super().setUp()
+
+    self.project = Project(name="TestProject", private_key="secret")
+    self.project.save()
+
+    self.headers = {
+      'HTTP_PUBLIC_KEY': self.project.id,
+      'HTTP_PRIVATE_KEY': self.project.private_key
+    }
+
+
+class UserTests(AuthenticatedAPITestCase):
+
+  def test_get_users(self):
+    response = self.client.get("/api/users/", {}, **self.headers)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class ChannelTests(AuthenticatedAPITestCase):
+
+  def test_create_channel(self):
+    data = {
+      'name': "TestChannel",
+      'meta': "TestMeta"
+    }
+
+    response = self.client.post("/api/channels/", data, **self.headers)
+
+    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
