9849ab0bfc9fae99798ac6aedcf8e811131e731e
Brent Strysko <brentstrysko@brentstrysko.com>
2014-12-28 11:42:31 -0500

    Removed dead root path tooltron urls(only crm/ remain until Tooltron upgrades to Public API).

--- robocrm/urls.py
+++ robocrm/urls.py
@@ -2,14 +2,6 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
-
-  # roboticsclub.org/crm
-  # TODO: remove these 3 patterns when site switched over
-  url(r'^roboauth/(?P<rfid_tag>[0-9A-Fa-f]+)/(?P<mach_num>\d+)/', views.roboauth),
-  url(r'^roboauth/(?P<rfid_tag>[0-9A-Fa-f]+)/', views.roboauthall),
-  url(r'^add_card_event/', views.add_card_event),
-
-  # roboticsclub.org/
   url(r'^crm/roboauth/(?P<rfid_tag>[0-9A-Fa-f]+)/(?P<mach_num>\d+)/', views.roboauth),
   url(r'^crm/roboauth/(?P<rfid_tag>[0-9A-Fa-f]+)/', views.roboauthall),
   url(r'^crm/add_card_event/', views.add_card_event),
