d219b29496b5cbde7be6ef13eb088137e48a4ef8
Brent Strysko <brentstrysko@brentstrysko.com>
2015-01-13 17:02:55 -0500

    Fixed error of trying to direct to RoboUser change admin.

--- crm/admin.py
+++ crm/admin.py
@@ -8,6 +8,11 @@ class UpdatedByAdmin(admin.ModelAdmin):
     obj.save()
 
   def updater_url(self, obj):
+
+    # RoboUsers have no change url
+    if type(obj.updater_object).__name__ == 'RoboUser':
+      obj.updater_object = obj.updater_object.user
+
     s = "admin:{}_{}_change".format(obj.updater_type.app_label, obj.updater_type.model)
     url = urlresolvers.reverse(s, args=(obj.updater_id, ))
     return '<a href="{}">{}</a>'.format(url, obj.updater_object)
