2e5a176298d09af4659e2aa42f0dfbf7b494418e
Brent Strysko <brentstrysko@brentstrysko.com>
2015-02-12 12:28:20 -0500

    Removed unused tooltron roboauthall url.

--- robocrm/urls.py
+++ robocrm/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = patterns('',
   url(r'^crm/roboauth/(?P<rfid_tag>[0-9A-Fa-f]+)/(?P<mach_num>\d+)/', views.roboauth),
-  url(r'^crm/roboauth/(?P<rfid_tag>[0-9A-Fa-f]+)/', views.roboauthall),
   url(r'^crm/add_card_event/', views.add_card_event),
 )
