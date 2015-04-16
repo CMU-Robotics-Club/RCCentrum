b7c45f57dcdc60f6338f049613ec86de5a16a0d3
Brent Strysko <brentstrysko@brentstrysko.com>
2015-04-16 14:56:27 -0400

    Updated officer label image size.

--- officers/label.py
+++ officers/label.py
@@ -4,7 +4,7 @@ def create_officer_label(officer):
   label = Label()
   name_start, name_end = label.add_text('#000000', (0,0), "Roboto-Thin.ttf", 200, officer.user.user.get_full_name())
   position_start, position_end = label.add_text('#000000', (int(name_end[0]/2), name_end[1]), "Roboto-Thin.ttf", 100, officer.position, center_x=True)
-  image_start, image_end = label.add_image((int(name_end[0]/2) - 250, position_end[1] + 50), (500, 500), officer.image.path)
+  image_start, image_end = label.add_image((int(name_end[0]/2) - 500, position_end[1] + 50), (1000, 1000), officer.image.path)
 
   memo_start, memo_end = label.add_text_split('#000000', (int(name_end[0]/2), image_end[1] + 50), "Roboto-Thin.ttf", 50, officer.memo, center_x=True, words_per_line=5)
 
